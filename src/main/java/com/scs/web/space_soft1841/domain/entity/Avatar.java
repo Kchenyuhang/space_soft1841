c917588123ca424032d24bce6e086671d2c7a84c
Kchenyuhang <kchenyuhang@gmail.com>
2019-12-03 09:04:15 +0800

    项目实体类更新

---
+++ src/main/java/com/scs/web/space_soft1841/domain/entity/Avatar.java
@@ -0,0 +1,15 @@
+package com.scs.web.space_soft1841.domain.entity;
+
+import lombok.Data;
+
+/**
+ * @ClassName Avatar
+ * @Description TODO
+ * @Author yh_chen
+ * @Date 2019/12/3
+ **/
+@Data
+public class Avatar {
+    private Integer avatarId;
+    private String avatar;
+}
